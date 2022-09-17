c5693cc2f0d7756bae96d360fb840f4c830a8a24
itsarsimo <s.abrami@itsrizzoli.it>
2022-09-17 12:31:27 +0200

    fix slider immagini

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,21 @@ const body = document.querySelector("body");
 btn.addEventListener("click", () =>{
     body.classList.toggle("open-menu");
 });
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("image-carousel");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
